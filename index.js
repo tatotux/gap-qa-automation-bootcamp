0ccff1c3d48f941f9428724deb3996d63d864b9d
Cristian Ruiz <christian.ruiz.otero@gmail.com>
2021-06-29 00:01:11 -0400

    problems 2 and 3 solved

--- index.js
+++ index.js
@@ -12,4 +12,33 @@ function getSumMultiples3_5(limit) {
     return sum;
 }
 
-console.log(getSumMultiples3_5(10));
+// console.log(getSumMultiples3_5(10));
+// console.log(getSumMultiples3_5(16));
+// console.log(getSumMultiples3_5(-4));
+
+// Problem 2
+
+function isNarcissistic(number){
+    let numDigits = number.toString().length;
+    let sum = 0; 
+    for(let i = 0; i < numDigits; i++) {
+        sum += number.toString()[i] ** numDigits;
+    }
+    return sum == number
+}
+
+// console.log(isNarcissistic(153));
+// console.log(isNarcissistic(1652));
+
+// Problem 3
+
+function maskify(text){
+    let visible = text.slice(-4);
+    return "#".repeat(text.slice(0,-4).length) + visible;
+}
+
+console.log(maskify("123456789"));
+console.log(maskify("Skippy"));
+
+// Problem 4
+
