6e90a9e65a01901e4ca1c1455ad81e557961b141
Cristian Ruiz <christian.ruiz.otero@gmail.com>
2021-06-29 15:58:12 -0400

    problems 4 and 5 solved

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ function getSumMultiples3_5(limit) {
     return sum;
 }
 
-// console.log(getSumMultiples3_5(10));
-// console.log(getSumMultiples3_5(16));
-// console.log(getSumMultiples3_5(-4));
+console.log("Tests problem 1");
+console.log(getSumMultiples3_5(10));
+console.log(getSumMultiples3_5(16));
+//console.log(getSumMultiples3_5(-4));
 
 // Problem 2
 
@@ -27,8 +28,9 @@ function isNarcissistic(number){
     return sum == number
 }
 
-// console.log(isNarcissistic(153));
-// console.log(isNarcissistic(1652));
+console.log("Tests problem 2");
+console.log(isNarcissistic(153));
+console.log(isNarcissistic(1652));
 
 // Problem 3
 
@@ -37,8 +39,43 @@ function maskify(text){
     return "#".repeat(text.slice(0,-4).length) + visible;
 }
 
+console.log("Tests problem 3");
 console.log(maskify("123456789"));
 console.log(maskify("Skippy"));
 
 // Problem 4
 
+function isPangram(text){
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    return alphabet.split("").every(x => text.indexOf(x) > -1);
+}
+
+console.log("Tests problem 4");
+console.log(isPangram("The quick brown fox jumpsover the lazy dog"));
+console.log(isPangram("Testing pangram"));
+
+// Problem 5
+
+function countDuplicates(text){
+    const textArray = text.toLowerCase().split("");
+    const countItems = {};
+    textArray.some((v, i, a) => {
+        if (a.lastIndexOf(v) !== i) {
+            countItems[v] = a.reduce((sum, ch) => ch == v ? sum + 1: sum, 0);
+        }
+    });
+    
+    if (Object.keys(countItems).length === 0) {
+        return "No characters repeats more than once";
+    } else {
+        for (let duplicate in countItems) {
+            console.log(`${duplicate} ocurrs ${countItems[duplicate]} times`);
+        }
+        return true;
+    }
+}
+
+console.log("Tests problem 5");
+console.log(countDuplicates("abcdaddda2234"));
+console.log(countDuplicates("abcd"));
+console.log(countDuplicates("aA11"));
