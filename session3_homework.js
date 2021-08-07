9bed186a49c425170b2ce5c1530d787b1546798d
Jose Garcia <jgarcia@growthaccelerationpartners.com>
2021-08-06 23:02:24 -0600

    PROBLEM 3 SOLVED

--- session3_homework.js
+++ session3_homework.js
@@ -1,6 +1,6 @@
 
 // PROBLEM 1
-multiples_3_5(1);
+multiples_3_5(10);
 
 function multiples_3_5(number){
     let suma = 0;
@@ -17,18 +17,31 @@ function multiples_3_5(number){
 
 
 // PROBLEM 2
-narcissisticNumbers(153)
+narcissisticNumbers(153);
 
 function narcissisticNumbers(number){
     let lengthNumber = `${number}`.length;
     let NewSum= 0;
-        
+
     String(number).split("").map((num)=>{
         NewSum += Math.pow(num,lengthNumber)  
     })
 
     if(number === NewSum) return console.log('true'); else {console.log('false')}
 }
+
+
+//PROBLEM 3
+maskedValue('Skippy');
+
+function maskedValue(UnMaskedValue) {  
+    const characters = UnMaskedValue.slice(-2)
+    return console.log(characters.padStart(UnMaskedValue.length, '#'));
+}
+
+
+
+
 
 
 
