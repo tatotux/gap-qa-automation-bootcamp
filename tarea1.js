52cff47a6f7318b3f9dbcf8aa4b8f55b509d76a6
lacastillo <lacastillo@ip-192-168-100-20.us-east-2.compute.internal>
2021-08-12 17:52:15 -0600

    Tarea #2: Javascript Basic Laura Castillo Martinez

--- tarea1.js
+++ tarea1.js
@@ -2,45 +2,60 @@
 //Problem 1
 
 const isMultiple = (number)=>{
-    let sum=0;
-    for(let i=1; i< number; i++){(i%3===0 || i%5===0)? sum+= i:0}
+    let sum = 0;
+    for(let i = 1; i < number; i++)
+    {
+    i % 3 === 0 || i % 5 === 0 ? sum += i : 0
+    }
     return sum;  
 }
 // Problem 2
 const isNarcissistic = (number) =>{
     let num= number.toString();
     let sum =0;
-    for (const i of num) {sum+= parseInt(i)** num.length;} return (number === sum) ;
+    for (const i of num)
+    {
+     sum+= parseInt(i)** num.length;
+    } 
+    return (number === sum) ;
 }
 
 //Problem 3
 const maskify = (text) => {
-    let mask= text.substr((text.length -4), text.length);
-    if(text.length > 4){ let toBeMask= '';
-    for (const i of text.substr(0, (text.length -4))){toBeMask = toBeMask + '#' ;}
+    let mask = text.substr((text.length -4), text.length);
+    if(text.length > 4)
+    { 
+    let toBeMask= '';
+        for (const i of text.substr(0, (text.length -4)))
+        { 
+            toBeMask = toBeMask + '#' ;
+        }
     mask = toBeMask.concat(mask);
-    } else {mask = text;} return mask;}
+    } else {mask = text;} 
+
+    return mask;}
 
 //Problem 4
 const isPangram = (text) => {
     let alphabet = "abcdefghijklmnopqrstuvwxyz";
-    let textLowercase = text.toLowerCase();
-    for (const i of alphabet) {if(textLowercase.indexOf(i)=== -1) return false ;} return true;
+    for (const i of alphabet) {
+        if( text.toLowerCase().indexOf(i) === -1) 
+        return false ;
+    } 
+    return true;
 }
 //Problem 5
-const isDuplicated = (text) => {
-let duplicated = [...text.toLowerCase()];
-let character = "abcdefghijklmnopqrstuvwxyz1234567890";
-let counter=0;
-for (const i of character) {(duplicated.indexOf (i)< duplicated.lastIndexOf(i)? counter++: counter); }
-return counter;}
+const countDuplicated = (text) => {
+const duplicated= [... text.toLowerCase()]
+    .filter((actual, index, array) => array.indexOf(actual) != array.lastIndexOf(actual) );
+return [... new Set (duplicated)].length;
+}
 
-
-const number= 1652;
-const text = "Laura Castillo Martinez 2255444";//
-console.log (Number.isInteger(number) && number > 0?isMultiple(number):0);
-console.log (Number.isInteger(number) && number > 0? isNarcissistic(number):undefined);
+const number= -153;
+const text = "abcde";//
+console.log (Number.isInteger(number) && number > 0 ? isMultiple(number) : 0);
+console.log (Number.isInteger(number) && number > 0 ? isNarcissistic(number) : "Can't be process");
 console.log (maskify(text));
 console.log(isPangram(text));
-console.log(isDuplicated(text));
+console.log(countDuplicated(text));
 
