d6371d26b9452efb2c181b42d5ee5e18721aa3ea
Cristina <cmanjarrez@growthaccelerationpartners.com>
2021-08-13 13:48:50 -0500

    Addding solution for Problems 4-5 and clearning code

--- jsHomework.js
+++ jsHomework.js
@@ -1,64 +1,74 @@
+//-------------------------Problem 1-------------------------------------
 function sumMultiples(num) {
-    let sum = 0;
-    let multiples = [];
+    let sum = 0,
+        multiples = [];
     for (let i = 1; i < num; i++) {
         if ((i % 3 == 0) || (i % 5 == 0)) {
             sum = sum + i;
             multiples.push(i);
-            // console.log("Es multiplo, num: ", i, "suma: " + sum);
         }
     }
     console.log("El listado de los multiplos es: " + multiples, " y su suma es: " + sum);
 }
 
-//sumMultiples(10);
-
-//-----------------------------------------------------------------------
+//-------------------------Problem 2-------------------------------------
 function isNarcicistic(num) {
-
-    var i = 0;
-    var sum = 0;
-    var splitDigits = num.toString().split('').map(Number);
+    var i = 0,
+        sum = 0;
+    const splitDigits = [...num.toString().split('')]
 
     while (i < splitDigits.length) {
         exp = Math.pow(splitDigits[i], splitDigits.length)
-            //console.log(exp);
         sum = sum + exp;
-        //console.log(sum);
         i++;
     }
     return (sum == num)
 }
 
-//console.log('Is it a Narsicistic number?: ' + isNarcicistic(153));
-
-//--------------------------------------------------------------------------
+//-----------------------------Problem 3-----------------------------------
 
 function maskify(data) {
     let newData = data.toString().split('');
     let i = 0;
-    console.log(newData.length);
     while (i < (newData.length - 4)) {
         newData[i] = '#';
-        //console.log(newData[i]);
         i++;
     }
     console.log('You data is :' + newData.join(''));
 }
+//----------------------------Problem 4-----------------------------------
 
-//maskify('Prueba de string mas largo');
-
-//---------------------------------------------------------------
 function isPanagram(words) {
+
     let alpha = 'abcdefghijklmnñopqrstuvwxyz';
-    let splitwords = words.toLowerCase().split('');
-    console.log(splitwords);
-    splitwords.forEach(element => {
-        for (element of alpha) {
-            let counter = +1;
-            console.log(counter);
+    let splitwords = words.toLowerCase();
+
+    if (splitwords.length < alpha.length) {
+        return false;
+    } else {
+        for (let i of alpha) {
+            if (splitwords.indexOf(i) === -1) {
+                return false;
+            } else {
+                return true;
+            }
         }
-    });
+    }
 }
 
-isPanagram("The quick brown fox jumps over the lazy dog");
+
+//---------------------------Problem 5-------------------------------
+const countDuplicates = (text) => {
+    let arr = text.toLowerCase().split('');
+    const unique = [...new Set(arr)];
+    let duplicates = arr.length - unique.length;
+    console.log('There are ' + duplicates + ' duplicates');
+}
+
+
+
+sumMultiples(10);
+console.log('Is it a Narsicistic number?: ' + isNarcicistic(153));
+maskify('Nananananananananananananananana Batman!');
+console.log('Is this a Panagram? ', isPanagram("The quick brown fox jumps over the lazy dog"));
+countDuplicates('indivisibility');
