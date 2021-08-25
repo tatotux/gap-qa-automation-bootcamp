a68a95b04e2770beb35565cedff274d0f513968b
lacastillo <lacastillo@lacastillos-MacBook-Pro.local>
2021-08-25 13:51:04 -0600

    Updating the code after Feedback

--- tarea1.js
+++ tarea1.js
@@ -1,59 +1,42 @@
 
 //Problem 1
-
-const isMultiple = (number)=>{
-    let sum = 0;
-    for(let i = 1; i < number; i++)
-    {
-    i % 3 === 0 || i % 5 === 0 ? sum += i : 0
-    }
-    return sum;  
+const sumMultiple = (number)=>{
+let num = [];
+for(let i=1; i <= number;i++) 
+{num.push(i);}
+return num.reduce((sum,actualValue)=> actualValue % 3 === 0 || actualValue % 5 === 0 ? sum += actualValue : sum = sum);   
 }
 // Problem 2
 const isNarcissistic = (number) =>{
-    let num= number.toString();
-    let sum =0;
-    for (const i of num)
-    {
-     sum+= parseInt(i)** num.length;
-    } 
-    return (number === sum) ;
+   
+    let sum =[... number.toString()].map((value, index, array)=> value ** array.length)
+    .reduce((sum,actual)=> sum +actual);
+       return (number == sum) ;
 }
 
 //Problem 3
 const maskify = (text) => {
-    let mask = text.substr((text.length -4), text.length);
-    if(text.length > 4)
-    { 
-    let toBeMask= '';
-        for (const i of text.substr(0, (text.length -4)))
-        { 
-            toBeMask = toBeMask + '#' ;
-        }
-    mask = toBeMask.concat(mask);
-    } else {mask = text;} 
-
-    return mask;}
+   let mask = [... text];
+   const toBeMask= mask.map((x, index, array)=> index < (array.length - 4) ? x = "#" : x = x)
+   .reduce((acc,current)=> acc.concat(current.toString()));
+   return toBeMask;
+}
 
 //Problem 4
 const isPangram = (text) => {
-    let alphabet = "abcdefghijklmnopqrstuvwxyz";
-    for (const i of alphabet) {
-        if( text.toLowerCase().indexOf(i) === -1) 
-        return false ;
-    } 
-    return true;
+let charOnthetext = [... new Set (text.replace(/\s+/g, ''))].length;
+return charOnthetext ===26 ?  true :  false ;
 }
 //Problem 5
 const countDuplicated = (text) => {
-const duplicated= [... text.toLowerCase()]
+const duplicated= [... text.toLowerCase().replace(/\s+/g, '')]
     .filter((actual, index, array) => array.indexOf(actual) != array.lastIndexOf(actual) );
 return [... new Set (duplicated)].length;
 }
 
-const number= -153;
-const text = "abcde";//
-console.log (Number.isInteger(number) && number > 0 ? isMultiple(number) : 0);
+const number= 153;
+const text ="Mr Jorck TV quiz PhiD bags few lynx";//
+console.log (Number.isInteger(number) && number > 0 ? sumMultiple(number) : 0);
 console.log (Number.isInteger(number) && number > 0 ? isNarcissistic(number) : "Can't be process");
 console.log (maskify(text));
 console.log(isPangram(text));
