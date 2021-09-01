01966a832cf2d0cd9b24a2dad7025905a3bb23b2
Jose Garcia <jgarcia@growthaccelerationpartners.com>
2021-08-31 18:12:24 -0600

    JOSEGARCIA_HOMEWORK2 inicial approach

--- homework2.js
+++ homework2.js
@@ -1,29 +1,63 @@
 const { default: axios } = require("axios");
+const { Table } = require('console-table-printer');
+const { printTable } = require('console-table-printer');
 
-getCurrency("")
 
+getCurrency("aaa")
 
 function getCurrency(currency =""){
-    let api_user_key= "13068c1d0972fdb467b4691f"
-    let host ="https://v6.exchangerate-api.com/v6/"
-
-    if (currency == ""){
-        host = `${host}${api_user_key}/codes` 
-     }else{
-        host = `${host}${api_user_key}/latest/${currency}` 
-     }
+   let api_user_key= "13068c1d0972fdb467b4691f"
+   let host ="https://v6.exchangerate-api.com/v6/" 
+   const currencyTable =  [];  
+   
+      if (currency == ""){
+         axios.get(`${host}${api_user_key}/codes`).then(response =>{         
+            for (let x in response.data.supported_codes) {            
+               currencyTable.push({Currency:response.data.supported_codes[x][0], Country:response.data.supported_codes[x][1]})                               
+            }  
+            console.table(currencyTable)
+      }) 
+      }else{
+       
+         axios.get(`${host}${api_user_key}/latest/${currency}`).then(response =>{
+           
+               for (let x in response.data.conversion_rates) {            
+                  currencyTable.push({Currency:x, Value:response.data.conversion_rates[x]})                               
+               }  
+               console.table(currencyTable)            
+         }).catch(e => {
+            console.log("Currency not Supported -> " + e)               
+   });
     
-     axios.get(`${host}`)
-        .then(response =>{
-            console.log(response.data)
-            
-        }).catch(e => {
-            console.log("Currency not Supported")               
-        });
-
-     
-
+      
+      } 
 }
+
+
+/*function getCurrency(currency =""){
+   let api_user_key= "13068c1d0972fdb467b4691f"
+   let host ="https://v6.exchangerate-api.com/v6/" 
+   const currencyTable =  [];
+  
+   if (currency == ""){
+      host = `${host}${api_user_key}/codes` 
+   }else{
+      host = `${host}${api_user_key}/latest/${currency}` 
+   }
+    
+   axios.get(`${host}`).then(response =>{
+         
+      for (let x in response.data.conversion_rates) {            
+         currencyTable.push({Currency:x, Value:response.data.conversion_rates[x]})                               
+      }           
+      
+      console.table(currencyTable)
+
+   }).catch(e => {
+            console.log("Currency not Supported -> " + e)               
+   });
+}*/
+
 
 /*function getCurrency(currency =""){
     var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
