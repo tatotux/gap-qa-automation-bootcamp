c4c39d4e28e4e54e7ff333563b6aa71fb9b20c43
Jonatan <jgarcia@square-root.com>
2021-09-03 14:26:21 -0500

    Cypress task session 1

--- cypress/integration/my-first-test.spec.js
+++ cypress/integration/my-first-test.spec.js
@@ -6,10 +6,11 @@ describe('As an admin user, I go to Insights', () => {
         cy.visit("http://ec2-100-25-33-224.compute-1.amazonaws.com:8000/")
     })
 
-    it('goes to the cart and verify it is empty', function() {
+    it('goes to the cart and verify it is empty.', function() {
         
        cy.get("ul.site-header-cart a").click()
        cy.contains("Your cart is currently empty.").should("be.visible")
+       
     
     })
 
