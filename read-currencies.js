2bb4b013a7afd27ef88d4c8217f1dcac5af1e733
Sonia Sibaja <ssibaja@wearegap.com>
2021-09-04 09:33:31 -0600

    Fixing currencies file and imolementing getFile as promise

--- read-currencies.js
+++ read-currencies.js
@@ -6,18 +6,36 @@ import fs from "fs";
  async function csvToArray(delimiter=','){
     
     try {
-        const str = await getFile();
-        const headers = str.slice(0, str.indexOf('\n')).split(delimiter);
-        const rows = str.slice(str.indexOf('\n')+1).split('\n');
+        // const str = getFile();
 
+        const str = await getFile
+                        .then(res =>{
+                            return res;
+                        })
+                        .catch(error =>{
+                            console.log(error);
+                        });
+
+        // var country = str.split("\r\n");
+        // console.log(country[1]);          
+
+
+
+        const headers = str.slice(0, str.indexOf('\r\n')).split(delimiter);
+        const rows = str.slice(str.indexOf('\r\n')+1).split('\r\n');
+
+
+
+        // Currency Code | Currency Name | Country
         const array = rows.map(function (row){
             const values = row.split(delimiter);
-            const el =headers.reduce(function(object, heaeder, index){
-                object[header] = values[index];
+            const el = headers.reduce(function(object, heaeders, index){
+                object[headers] = values[index];
                 return object;
             }, {});
             return el;
         });
+        console.log(array);
         return array;
     } catch (error) {
         console.log(error);
@@ -25,17 +43,46 @@ import fs from "fs";
     
 }
 
+// function getFile() {
+//     try {
+//         let buf = new Buffer.alloc(5500);
+//         // Supported currencies are stored in a csv file
+//         // Opening csv file, reading and closing or properly
+//         fs.open('//Users/adminsv/Documents/QABootcamp/gap-qa-automation-bootcamp/currencies.csv', 'r+', function(err, fd) {
+//             if (err) {
+//                 return console.error(err);
+//             }
+            
+//             fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
+//                 if (err){
+//                     console.log(err);
+//                 }
 
-async function getFile() {
+//                 if(bytes > 0){
+//                     const file = buf.slice(0, bytes).toString();
+//                     // console.log(buf.slice(0, bytes).toString());
+//                     return file;
+                    
+//                 }
+//             });
+//         })
+        
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// // getFile();
+
+const getFile = new Promise((resolve, reject) => {
     try {
-        let buf = new Buffer.alloc(1024);
-        // Supported currencies are stored in a csv file
-        // Opening csv file, reading and closing or properly
-        fs.open('/Users/adminsv/Documents/QABootcamp/gap-qa-automation-bootcamp/currecies.csv', 'r', function(err, fd) {
+        let buf = new Buffer.alloc(5500);
+
+        // Supported currencies are stored in a csv file. Opening csv file, reading and closing it properly
+        fs.open('//Users/adminsv/Documents/QABootcamp/gap-qa-automation-bootcamp/currencies.csv', 'r', function(err, fd) {
             if (err) {
                 return console.error(err);
-            }
-            
+            } 
+        
             fs.read(fd, buf, 0, buf.length, 0, function(err, bytes){
                 if (err){
                     console.log(err);
@@ -43,8 +90,9 @@ async function getFile() {
 
                 if(bytes > 0){
                     const file = buf.slice(0, bytes).toString();
-                    return file;
-                    //console.log(buf.slice(0, bytes).toString());
+                    // console.log(buf.slice(0, bytes).toString());
+                    resolve(file);
+                    
                 }
             });
         })
@@ -52,8 +100,14 @@ async function getFile() {
     } catch (error) {
         console.log(error);
     }
-}
-
+});
+ 
+//  Ths is working fine
+// getFile
+// .then(res =>{
+//     console.log(' ====== CSV FILE RETURNED ======');
+//     console.log(res);
+// });
 
 console.log(csvToArray());
 
