a9cb6441c32bb7941279314e386e1ec727447958
Sonia Sibaja <ssibaja@wearegap.com>
2021-09-05 01:36:25 -0600

    Continue exhange rate implementation and cleaning up

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
 import axios from 'axios';
+import {csvToArray} from './get-country-currencies.js';
+ 
+const getAllExchangeRates =  currencyCode => {
 
-// axios({
-//     method:'GET',
-//     url: 'https://v6.exchangerate-api.com/v6/0b7f1a21180a722c55a6d05b/latest/',
-//     responseType: 'json'
-// })
-
-
-
-const getConversionRates = async (currencyCode) => {
-     return await new Promise((resolve, reject) => {
-
-        //value >10 ? resolve (''), reject('');   EJEMPLO DE LA CLASE
-        resolve (
+    return new Promise((resolve, reject) => {
 
             axios({
                 method:'GET',
@@ -23,49 +14,43 @@ const getConversionRates = async (currencyCode) => {
 
             axios.get(currencyCode)
             .then( function (response){
-
-                /// aqui la carnita de loq ue debería hacer con el response
-
-                console.log('    ==================  DATA  ==========================');
-
+            
                 if (response.status === 200){
-                    // console.log(response.data);
                     const data = response.data;
-                    console.log(typeof data);
-                    console.log(data['conversion_rates']);
-
-                    console.log('    ==================  CURRENCIES  ==========================');
-
-                    const conversionRates = data['conversion_rates'];
-                    console.log(conversionRates.AUD);
-                } 
-                
-                
-
+                    resolve(data);
+                }
             })
             .catch(function(error){
-                 if (error.request) {
-                    // The request was made but no response was received
-                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                    // http.ClientRequest in node.js
-                    console.log(error.request);
-                  } else {
-                    // Something happened in setting up the request that triggered an Error
-                    console.log('Error', error.message);
-                  }
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                // OR Something happened in setting up the request that triggered an Error
+                error.request ? console.log(error.request) : console.log('Error', error.message);
+                reject(error);
+                  
             })
             .then(function(){})
-        )
-        reject((e) => console.log(e))
-
     });
 };
 
-//con await y async yo no necesito el then - carch para resolver la promesa, 
-//con await y async se seresuelven automaticamente
+//con await y async la promesa se seresuelve automaticamente. no necesito el then - catch
 async function getExchangeRate(currencyCode) {
     try {
-        const data = await getConversionRates(currencyCode);
+        const respnse = await getAllExchangeRates(currencyCode);
+        const exchangeRates = respnse.conversion_rates;
+        console.log('===========================================================================');
+        console.log(exchangeRates);
+
+        console.log(' ======================= LLAMANDO LA PROMESA CSVTOARRAY');
+        const countries = await csvToArray()
+                                .then(res =>{
+                                    return(res);
+                                })
+                                .catch(error =>{
+                                    console.log(error);
+                                });
+        console.log(countries);
+
     } catch (e) {
         console.log(e)
     }
