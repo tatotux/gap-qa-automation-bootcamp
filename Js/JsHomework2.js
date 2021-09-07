2fa274627a7cab7aa801d91b923625290cbfb8c6
Cristina <cmanjarrez@growthaccelerationpartners.com>
2021-09-06 21:21:21 -0500

    Added the enriched Api endpoint to support countries and added solution to get empy or not supported currency

--- Js/JsHomework2.js
+++ Js/JsHomework2.js
@@ -2,53 +2,46 @@ console.log(getRate('usd'));
 
 
 async function getRate(cur) {
-    const data = await getApiData();
-    //const country = await getCountry();
-    //console.log(countries);
-    let rates = data.rates;
-    let found = false;
+
     if (cur) {
         currency = cur.toUpperCase();
-        for (key of Object.keys(rates)) {
-            if (currency === key) {
-                console.log(`The exchange rate for ${currency} is ${rates[key]  }`);
-                found = true;
-                break;
+        //Get data from the API
+        let currencyInfo = await getInfo(currency);
+        if (currencyInfo.result !== 'error') {
+            let data = currencyInfo.target_data;
+            let display = {
+                'currency_code': currencyInfo.target_code,
+                'currency_name': data.currency_name,
+                'country': data.locale,
+                'conversaion_rate': currencyInfo.conversion_rate
             }
-        }
-        if (found === false)
+            console.table({ display });
+        } else {
             console.log(`The currency ${currency} is not supported.`);
-    } else
-        console.log('This are all the available currency rates', data.rates);
+        }
+
+    } else displayAll();
 };
 
+//Function to get all data from the api 
 async function getApiData() {
     let url = 'https://open.er-api.com/v6/latest/USD';
     let response = fetch(url)
     return (await response).json();
 }
 
-async function getApiCountry() {
-    api_key = 'd4592897fdbb98bbe5b48709';
-    let url = 'https://v6.exchangerate-api.com/v6/d4592897fdbb98bbe5b48709/codes';
-    let response = fetch(url);
+//Function to get data from the api based on a single currency
+async function getInfo(cur) {
+    const api_key = 'd4592897fdbb98bbe5b48709';
+    let url = 'https://v6.exchangerate-api.com/v6/' + api_key + '/enriched/USD/' + cur;
+    let response = fetch(url)
     return (await response).json();
 
-}
+};
 
-async function findCountry(cur) {
-    const country = await getApiCountry();
-    let countries = country.supported_codes;
-    let found = Object.keys(countries)
-    found.filter(function(element) {
-        if (element === cur)
-            console.log()
-        return countries[key];
-    });
-
-    // let rateCountry = countries[key];
-    //console.log(found);
-
-
-}
-console.log(findCountry('USD'))
+//Function to display all data
+async function displayAll() {
+    const data = await getApiData();
+    let rates = data.rates;
+    console.table(data.rates);
+};
