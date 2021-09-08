e28d405e84e3cf63762613a5878ba0d01825d32f
Ronald Ramirez <rramirez@wearegap.com>
2021-09-07 19:35:39 -0600

    Adding Javascript and Cypress homework

---
+++ cypress/integration/add-to-cart.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Add to cart', () => {
+
+    beforeEach(() => {
+        cy.visit('http://ec2-100-25-33-224.compute-1.amazonaws.com:8000/');
+    });
+
+    it('should add to cart', () => {
+        cy.get('#masthead .search-field').type('Belt{enter}');
+    });
+});
