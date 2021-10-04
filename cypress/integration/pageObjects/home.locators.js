cec71e391a03e68263d961356f584bd28833c411
Mario Rodriguez <>
2021-10-04 17:20:41 -0600

    Cypress homework version 1,  pending to organize the code and improve the solution

---
+++ cypress/integration/pageObjects/home.locators.js
@@ -0,0 +1,22 @@
+export const playgroundElements = {
+
+    searchProductBar: '[id="woocommerce-product-search-field-0"]',
+    searchProductsCount: '[class="woocommerce-result-count"]',
+
+    addToCart: '[class="single_add_to_cart_button button alt"]',
+    viewCart: '[class="cart-contents"]',
+
+    checkoutButton: '[class="checkout-button button alt wc-forward"]',
+    firstName: '[name="billing_first_name"]',
+    lastName: '[name="billing_last_name"]',
+    billingAddress: '[name="billing_address_1"]',
+    billingCity: '[name="billing_city"]',
+    billingPostCode: '[name="billing_postcode"]',
+    billingPhone: '[name="billing_phone"]',
+    billingEmail: '[name="billing_email"]',
+    useDiffShipAddress: '[name="ship_to_different_address"]',
+
+    termsAndConditions: '[class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox"]',
+    placeOrder: '[name="woocommerce_checkout_place_order"]',
+    orderReceived: '[class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"]'
+}
